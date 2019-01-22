862d69247ee995f1de34dfb64850e1c9c585268c
JaoK <JJaoKK@gmail.com>
2019-01-22 13:21:29 +0100

    Create Saab95.java

---
+++ w04/Saab95.java
@@ -0,0 +1,70 @@
+import javafx.scene.paint.Color;
+
+public class Saab95 {
+
+	public boolean turboOn;
+
+	public double enginePower; // Engine power of the car
+	public double currentSpeed; // The current speed of the car
+	public Color color; // Color of the car
+	public String modelName; // The car model name
+
+	public Saab95() {
+		color = Color.BLACK;
+		enginePower = 125;
+		turboOn = false;
+		modelName = "Saab95";
+		stopEngine();
+	}
+
+	public double getEnginePower() {
+		return enginePower;
+	}
+
+	public double getCurrentSpeed() {
+		return currentSpeed;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void startEngine() {
+		currentSpeed = 0.1;
+	}
+
+	public void stopEngine() {
+		currentSpeed = 0;
+	}
+
+	public void setTurboOn() {
+		turboOn = true;
+	}
+
+	public void setTurboOff() {
+		turboOn = false;
+	}
+
+	public double speedFactor() {
+		double turbo = 1;
+		if (turboOn)
+			turbo = 1.3;
+		return enginePower * 0.01 * turbo;
+	}
+
+	public void incrementSpeed(double amount) {
+		currentSpeed = getCurrentSpeed() + speedFactor() * amount;
+	}
+
+	public void decrementSpeed(double amount) {
+		currentSpeed = getCurrentSpeed() - speedFactor() * amount;
+	}
+
+	public void gas(double amount) {
+		incrementSpeed(amount);
+	}
+
+	public void brake(double amount) {
+		decrementSpeed(amount);
+	}
+}
