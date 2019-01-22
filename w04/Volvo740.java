45c72fda4213a58156177c152ac0bdac2338ad59
JaoK <JJaoKK@gmail.com>
2019-01-22 13:21:26 +0100

    Create Volvo740.java

---
+++ w04/Volvo740.java
@@ -0,0 +1,58 @@
+import javafx.scene.paint.Color;
+
+public class Volvo740 {
+
+	public final static double trimFactor = 1.25;
+
+	public double enginePower; // Engine power of the car
+	public double currentSpeed; // The current speed of the car
+	public Color color; // Color of the car
+	public String modelName; // The car model name
+
+	public Volvo740() {
+		color = Color.RED;
+		enginePower = 100;
+		modelName = "Volvo740";
+		stopEngine();
+	}
+
+	public double getEnginePower() {
+		return enginePower;
+	}
+
+	public double getCurrentSpeed() {
+		return currentSpeed;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void startEngine() {
+		currentSpeed = 0.1;
+	}
+
+	public void stopEngine() {
+		currentSpeed = 0;
+	}
+
+	public double speedFactor() {
+		return enginePower * 0.01 * trimFactor;
+	}
+
+	public void incrementSpeed(double amount) {
+		currentSpeed = getCurrentSpeed() + speedFactor() * amount;
+	}
+
+	public void decrementSpeed(double amount) {
+		currentSpeed = getCurrentSpeed() - speedFactor() * amount;
+	}
+
+	public void gas(double amount) {
+		incrementSpeed(amount);
+	}
+
+	public void brake(double amount) {
+		decrementSpeed(amount);
+	}
+}
